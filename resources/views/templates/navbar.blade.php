c5b3cf37d6c8f361df9174fd67e97fcd88a37707
WolfLonely408 <142439949+LucasGabriel408@users.noreply.github.com>
2025-08-13 23:35:25 -0300

    CSS nav bar

--- resources/views/templates/navbar.blade.php
+++ resources/views/templates/navbar.blade.php
@@ -1,31 +1,29 @@
-<nav>
+<nav class="nav-bar">
     <div class="container-nav">
-        <img src="" alt="" class="nav-logo">
+        <img src="/images/logo.jpg" alt="" class="nav-logo">
     </div>
     <div class="container-nav">
-        <ul class="nav-links">
-            <li><a href="{{ route('home') }}">Home</a></li>
-            <li><a href="{{ route('movies') }}">Filmes</a></li>
-            <li><a href="{{ route('manage') }}">Gerenciar</a></li>
-            <div class="profile">
-                @if (Auth::check())
-                    <img src="{{ Auth::user()->photo }}" alt="" class="profile-photo">
-                    <!-- An arrow icon to expand the menu -->
-                    <i class="icon"></i>
-                    <div class="menu" hidden>
-                        <ul>
-                            <li><a href="{{ route('profile') }}">Perfil</a></li>
-                            <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sair</a></li>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </ul>
-                    </div>
-                @else
-                    <li><a href="{{ route('login') }}">Entrar</a></li>
-                    <li><a href="{{ route('register') }}">Registrar</a></li>
-                @endif
-            </div>
-        </ul>
+        <a href="{{ route('home') }}">Home</a>
+        <a href="{{ route('movies') }}">Filmes</a>
+        <a href="{{ route('manage') }}">Gerenciar</a>
+        <div class="profile">
+            @if (Auth::check())
+                <img src="{{ Auth::user()->photo }}" alt="" class="profile-photo">
+                <!-- An arrow icon to expand the menu -->
+                <i class="icon"></i>
+                <div class="menu" hidden>
+                    <a href="{{ route('profile') }}">Perfil</a>
+                    <a href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sair</a>
+                    
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            @else
+                <a href="{{ route('login') }}">Entrar</a>
+                <a href="{{ route('register') }}">Registrar</a>
+            @endif
+        </div>
     </div>
 </nav>
